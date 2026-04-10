7cf25670ad560b28fa5971d0a74233ddc71a842b
antonellallanten-sys <antonella.llanten@mail.udp.cl>
2026-04-10 09:25:58 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,162 @@
+ function setup() {
+  createCanvas(560, 755);
+}
+
+function draw() {
+  background(20, 30, 80);
+  
+  noStroke();
+  
+  
+  //FIGURAS 
+  
+  //rectangulo blanco arriba
+
+  fill(255);
+  rect(0, 0, 80, 130);
+  
+  //rectangulo blanco arriba horizontal
+  
+  fill(255);
+  rect(160, 0, 180, 60);
+  
+  //rectangulo amarillo arriba
+  
+  fill (255, 200, 0);
+  rect (160, 0, 50, 130); 
+  
+  //rectangulo celeste arriba horizontal
+  
+  fill(80, 140, 200);
+  rect(330, 0 , 130, 60);
+  
+  //rectangulo celeste abajo
+  
+  fill(80, 140, 200);
+  rect (0, 130, 210, 55);
+  
+  //rectangulo amarillo 
+  fill(255, 200, 0);
+  rect( 0, 185, 80, 130);
+  
+  //rectangulo 1 y 2 blanco
+  fill (225,225, 225);
+  rect (160, 185, 50, 60);
+  
+  fill (225, 225, 225);
+  rect (120, 200, 90, 50);
+  
+  //rectangulo horizontal amarillo
+  
+  fill( 255, 200 ,0);
+  rect(210, 185, 130, 65);
+  
+  //rectangulo vertical blanco
+  
+  fill(225, 225, 225,);
+  rect (340, 185, 50, 175)
+
+
+//rectangulo abajo celeste
+
+fill(80, 140, 200);
+rect(120, 250, 155, 65);
+
+  //rectangulo rojo vertical 1
+  
+  fill(200, 40, 40);
+  rect(275, 250, 65, 155);
+  
+  //rectangulo L celeste
+  
+  fill(80, 140, 200);
+  rect(0, 315, 65, 170);
+  
+  fill(80, 140, 200);
+  rect(0, 480, 250, 60);
+  
+  //conjunto rectangulo amarillo y blanco
+  
+  fill(255, 200, 0);
+  rect (120, 315, 40, 165)
+  
+fill(225, 225, 225);
+  rect(160, 315, 20, 165);
+  
+  //rectangulo rojo 2
+  
+  fill(200, 40, 40);
+  rect(210, 410, 40, 70);
+  
+  //cojuto de L celeste pequeña
+  
+  fill(80, 140, 200);
+  rect (275, 400, 65, 140);
+  fill(80, 140, 200);
+  rect(275, 480, 115, 60);
+  
+  //conjunto rectanglos amarillos
+  
+  fill(255, 200, 0);
+  rect (340, 360, 50, 120);
+  fill(255, 200, 0);
+  rect( 340, 360, 100, 30);
+  fill(255, 200, 0);
+  rect(415, 360, 25, 120)
+  
+  //rectangulo blanco continuo
+  fill(225, 225, 225);
+  rect (435, 360, 50, 120);
+  
+  //rectangulo celeste grande
+  fill(80, 140, 200);
+  rect(485, 390, 75, 260);
+  
+  //conjunto L amarillo
+  fill(255, 200, 0);
+  rect(170, 540, 35, 90);
+  fill(255, 200, 0);
+  rect(0, 590, 200, 40);
+  
+  //rectangulos celestes abajo
+  fill(80, 140, 200);
+  rect(0, 630, 70, 40);
+  fill (80, 140, 200);
+  rect(0, 630, 40, 70);
+  fill (80, 140, 200);
+  rect(0, 700, 70, 40);
+  fill(80, 140, 200);
+  rect(205, 540, 70, 130);
+  fill(80, 140, 200);
+  rect(0, 700, 190, 40);
+  fill(80, 140, 200);
+  rect(300, 590, 190, 50);
+  fill(80, 140, 200);
+  rect(300, 680, 260, 30);
+  
+  
+  //conjunto rectangulos blancos de abajo
+  
+  fill(225, 225, 225);
+  rect(70, 630, 135, 40);
+  
+  fill(225, 225, 225);
+  rect(275, 560, 60, 110);
+  
+  //conjunto rectangulos amarillos de abajo
+  
+  fill(255, 200, 0);
+  rect(335, 540, 150, 50);
+  fill(255, 200, 0);
+  rect(440, 540, 45, 110);
+  fill(255, 200, 0);
+  rect(335, 630, 120, 20);
+  
+  fill(255, 200, 0);
+  rect(170, 680, 165, 60);
+  
+  
+  
+  
+  
+}
